d303ab4e99d9eb966e593ee8abcd905faa236d76
Alan Frindell <afrind@meta.com>
2025-01-19 08:27:43 -0800

    Implement announce using Subscriber interface (#17)
    Summary:
    Pull Request resolved: https://github.com/facebookexperimental/moxygen/pull/17
    
    By removing the last use of the control message queue, delete all control loops and control visitors
    
    Differential Revision: D68139012

--- moxygen/relay/MoQRelay.h
+++ moxygen/relay/MoQRelay.h
@@ -15,6 +15,7 @@
 namespace moxygen {
 
 class MoQRelay : public Publisher,
+                 public Subscriber,
                  public std::enable_shared_from_this<MoQRelay>,
                  public MoQForwarder::Callback {
  public:
@@ -29,10 +30,9 @@ class MoQRelay : public Publisher,
   folly::coro::Task<SubscribeAnnouncesResult> subscribeAnnounces(
       SubscribeAnnounces subAnn) override;
 
-  void onAnnounce(Announce&& ann, std::shared_ptr<MoQSession> session);
-  void onUnannounce(
-      Unannounce&& ann,
-      const std::shared_ptr<MoQSession>& session);
+  folly::coro::Task<Subscriber::AnnounceResult> announce(
+      Announce ann,
+      std::shared_ptr<Subscriber::AnnounceCallback>) override;
 
   void removeSession(const std::shared_ptr<MoQSession>& session);
 
@@ -47,27 +47,54 @@ class MoQRelay : public Publisher,
       TrackNamespace prefix,
       std::shared_ptr<MoQSession> session);
 
-  struct AnnounceNode {
-    folly::F14NodeMap<std::string, AnnounceNode> children;
+  struct AnnounceNode : public Subscriber::AnnounceHandle {
+    explicit AnnounceNode(MoQRelay& relay) : relay_(relay) {}
+
+    void unannounce() override {
+      relay_.unannounce(announceOk().trackNamespace, this);
+    }
+
+    using Subscriber::AnnounceHandle::setAnnounceOk;
+
+    folly::F14FastMap<std::string, std::shared_ptr<AnnounceNode>> children;
+    // Sessions with a SUBSCRIBE_ANNOUNCES here
     folly::F14FastSet<std::shared_ptr<MoQSession>> sessions;
+    // All active ANNOUNCEs for this node (includes prefix sessions)
+    folly::
+        F14FastMap<std::shared_ptr<MoQSession>, std::shared_ptr<AnnounceHandle>>
+            announcements;
+    // The session that ANNOUNCEd this node
     std::shared_ptr<MoQSession> sourceSession;
+    MoQRelay& relay_;
   };
-  AnnounceNode announceRoot_;
-  AnnounceNode* findNamespaceNode(
+  AnnounceNode announceRoot_{*this};
+  std::shared_ptr<AnnounceNode> findNamespaceNode(
       const TrackNamespace& ns,
       bool createMissingNodes,
       std::vector<std::shared_ptr<MoQSession>>* sessions = nullptr);
   std::shared_ptr<MoQSession> findAnnounceSession(const TrackNamespace& ns);
 
   struct RelaySubscription {
+    RelaySubscription(
+        std::shared_ptr<MoQForwarder> f,
+        std::shared_ptr<MoQSession> u)
+        : forwarder(std::move(f)), upstream(std::move(u)) {}
+
     std::shared_ptr<MoQForwarder> forwarder;
     std::shared_ptr<MoQSession> upstream;
-    SubscribeID subscribeID;
+    SubscribeID subscribeID{0};
     std::shared_ptr<Publisher::SubscriptionHandle> handle;
     folly::coro::SharedPromise<folly::Unit> promise;
   };
 
   void onEmpty(MoQForwarder* forwarder) override;
+
+  folly::coro::Task<void> announceToSession(
+      std::shared_ptr<MoQSession> session,
+      Announce ann,
+      std::shared_ptr<AnnounceNode> nodePtr);
+
+  void unannounce(const TrackNamespace& trackNamespace, AnnounceNode* node);
 
   TrackNamespace allowedNamespacePrefix_;
   folly::F14FastMap<FullTrackName, RelaySubscription, FullTrackName::hash>
